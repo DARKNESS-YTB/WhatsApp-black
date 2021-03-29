599f00c10c97229c208a32590c1d663deb2a6e1e
DARKNESS-YTB <78645960+DARKNESS-YTB@users.noreply.github.com>
2021-03-29 20:12:26 +0200

    Add files via upload

--- Bot/root.sh
+++ Bot/root.sh
@@ -1,1 +1,15 @@
+#unknown
+#Black-Mafia-2
+#DARKNESS-YT
+#DARK-STORM
+#BOT-LR
+echo "KALI LINUX"
+echo "TERMUX"
+echo "LINUX"
+echo "DARK STORM"
+echo "DARKNESS-YT CHANNEL"
+cd
+cd WhatsApp-black
+cd wood
+sh wood.sh
 
